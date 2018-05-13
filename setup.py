c1c1e7ddf940f1886c66528ffa994cec7ed5b2cd
Jan Katins <jasc@gmx.net>
2018-05-13 17:59:48 +0200

    Initial working version of the package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mara-cli',
+    version='0.1',
+
+    description="Mara cli app which calls the appropriate contributed subcommand.",
+
+    install_requires=[
+        'mara-config>=0.1'],
+
+    dependency_links=[
+        'git+https://github.com/mara/mara-config.git@master#egg=mara-config',
+    ],
+
+    extras_require={
+        'test': ['pytest'],
+    },
+
+    packages=find_packages(),
+
+    author='Mara contributors',
+    license='MIT',
+
+    entry_points={
+        'console_scripts': [
+            'mara = mara_cli.cli:main',
+        ],
+    },
+
+)
